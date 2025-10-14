d9dcb162a20687ca7575421b033efa7db4b2b00a
Felix Nuworsu <nuworsufelix49@gmail.com>
2025-10-14 13:11:22 -0500

    refactor: fixed path for docker run

--- run.sh
+++ run.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-# run-hhvm.sh
-# Runs a Hack/HHVM script inside an official HHVM Docker container
-# Works on macOS (M1–M4) using amd64 emulation
 
-# Exit immediately if a command fails
 set -e
 
-# Check for the script argument
 if [ -z "$1" ]; then
   echo "Usage: $0 <hack-file>"
   exit 1
 fi
 
 SCRIPT_PATH="$1"
+CURRENT_DIR="$(pwd)"
 
-# Run HHVM inside Docker
 docker run --rm -it \
   --platform linux/amd64 \
-  -v "$(pwd)":/app -w /app \
+  --mount type=bind,source="${CURRENT_DIR}",target=/app \
+  -w /app \
   hhvm/hhvm:latest hhvm "$SCRIPT_PATH"
