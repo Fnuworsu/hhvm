4a1158c6fe06c829e213a98c0d21eb41ab6e7e42
Felix Nuworsu <nuworsufelix49@gmail.com>
2026-04-16 21:35:35 -0500

    edit run script

--- run.sh
+++ run.sh
@@ -9,9 +9,23 @@ fi
 
 SCRIPT_PATH="$1"
 CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
-docker run --rm -it \
-  --platform linux/amd64 \
-  --mount type=bind,source="${CURRENT_DIR}",target=/app \
-  -w /app \
-  hhvm/hhvm:latest hhvm "$SCRIPT_PATH"
+# If running a messagingBroker script, include all dependencies
+if [[ "$SCRIPT_PATH" == *"messagingBroker"* ]]; then
+  docker run --rm -it \
+    --platform linux/amd64 \
+    --mount type=bind,source="${CURRENT_DIR}",target=/app \
+    -w /app \
+    hhvm/hhvm:latest hhvm \
+    messagingBroker/queueService.hack \
+    messagingBroker/exchangeService.hack \
+    messagingBroker/loadBalancer.hack \
+    messagingBroker/main.hack
+else
+  docker run --rm -it \
+    --platform linux/amd64 \
+    --mount type=bind,source="${CURRENT_DIR}",target=/app \
+    -w /app \
+    hhvm/hhvm:latest hhvm "$SCRIPT_PATH"
+fi
